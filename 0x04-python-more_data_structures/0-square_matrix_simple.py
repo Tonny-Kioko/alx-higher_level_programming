f9742cba58068af5ff56b4f847ea0aedd89c3310
Tonny <anthonykioko@gmail.com>
2022-09-01 13:17:36 -0700

    updates

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+    matr = []
+    for x in matrix:
+        matr.append(list(map(lambda x: x**2, x)))
+    return (matr)
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            copy.append(replace)
-        else:
-            copy.append(my_list[i]
-    return copy
+    return(list(map(lambda x: replace if x is search else x, my_list)))
